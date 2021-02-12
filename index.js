9baacfc417d458454206029355bad464535c86b3
bsohangpur <47111080+bsohangpur@users.noreply.github.com>
2021-02-12 17:33:38 +0530

    update on 12-2-21

---
+++ index.js
@@ -0,0 +1,60 @@
+const num1=document.getElementById('1');
+num1.addEventListener('click',function(){
+  document.body.style.backgroundColor="red";
+})
+const num2=document.getElementById('2');
+num2.addEventListener('click',function(){
+  document.body.style.backgroundColor="green";
+})
+const num3=document.getElementById('3');
+num3.addEventListener('click',function(){
+  document.body.style.backgroundColor= "blue";
+})
+const num4=document.getElementById('4');
+num4.addEventListener('click',function(){
+  document.body.style.backgroundColor="orange";
+})
+const num5=document.getElementById('5');
+num5.addEventListener('click',function(){
+  document.body.style.backgroundColor="ivory";
+})
+const num6=document.getElementById('6');
+num6.addEventListener('click',function(){
+  document.body.style.backgroundColor= "gray";
+})
+const num7=document.getElementById('7');
+num7.addEventListener('click',function(){
+  document.body.style.backgroundColor="#000099";
+})
+const num8=document.getElementById('8');
+num8.addEventListener('click',function(){
+  document.body.style.backgroundColor="pink";
+})
+const num9=document.getElementById('9');
+num9.addEventListener('click',function(){
+  document.body.style.backgroundColor= "aqua";
+})
+const num10=document.getElementById('10');
+num10.addEventListener('click',function(){
+  document.body.style.backgroundColor="#993333";
+})
+const num11=document.getElementById('11');
+num11.addEventListener('click',function(){
+  document.body.style.backgroundColor="#999933";
+})
+const num12=document.getElementById('12');
+num12.addEventListener('click',function(){
+  document.body.style.backgroundColor= "yellow";
+})
+const num13=document.getElementById('13');
+num13.addEventListener('click',function(){
+  document.body.style.backgroundColor="coral";
+})
+const num14=document.getElementById('14');
+num14.addEventListener('click',function(){
+  document.body.style.backgroundColor="burlywood";
+})
+const num15=document.getElementById('15');
+num15.addEventListener('click',function(){
+  document.body.style.backgroundColor= "gold";
+})
